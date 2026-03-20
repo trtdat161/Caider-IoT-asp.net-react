d1fa09ea7135596b77255bdfc190927fcf7ba544
Tran Tien Dat <trtdat161@gmail.com>
2026-03-20 15:44:44 +0700

    fix: css import case sensitive

--- Caider-frontend/src/home/CaiderScan.jsx
+++ Caider-frontend/src/home/CaiderScan.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "../css/CaiderScan.css";
+import "../css/caiderScan.css";
 
 export default function CaiderScan() {
   const [particles, setParticles] = useState([]);
