ba6d9be28154fb87446b0d5efa3af1394d210ee9
Tran Tien Dat <trtdat161@gmail.com>
2026-04-01 21:56:07 +0700

    fix css

--- Caider-frontend/src/main.jsx
+++ Caider-frontend/src/main.jsx
@@ -1,5 +1,5 @@
 import "./css/reset.css";
-import "./css/style.css"; // chứa font
+import "./css/dashboard.css"; // chứa font
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
